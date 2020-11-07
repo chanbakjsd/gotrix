24b4625ced1932d8f07cce7a151a0e65c69504d7
Chan Wen Xu <lutherchanpublic@gmail.com>
2020-11-07 21:17:05 +0800

    feat: Implement simple message sending

---
+++ send.go
@@ -0,0 +1,25 @@
+package gomatrix
+
+import (
+	"github.com/chanbakjsd/gomatrix/event"
+	"github.com/chanbakjsd/gomatrix/matrix"
+)
+
+// SendMessage sends a message to the provided room ID with the provided content.
+func (c *Client) SendMessage(roomID matrix.RoomID, content string) (matrix.EventID, error) {
+	return c.RoomEventSend(roomID, event.TypeRoomMessage, event.RoomMessage{
+		MsgType: event.RoomMessageText,
+		Body:    content,
+	})
+}
+
+// SendNotice sends a notice to the provided room ID with the provided content.
+//
+// Notice are the same as messages except they're not intended to be parsed by bots (ie. other bots'
+// messages).
+func (c *Client) SendNotice(roomID matrix.RoomID, content string) (matrix.EventID, error) {
+	return c.RoomEventSend(roomID, event.TypeRoomMessage, event.RoomMessage{
+		MsgType: event.RoomMessageText,
+		Body:    content,
+	})
+}
